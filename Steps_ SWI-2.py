b7da879687324ada9f8c49cc6a1129f74055c96f
Turppie <125225459+Turppie@users.noreply.github.com>
2023-11-21 21:46:22 -0500

    Create Steps_ SWI-2.py

---
+++ Steps_ SWI-2.py
@@ -0,0 +1,11 @@
+def Given_El_usuario_tiene_una_cuenta_en_el_sistema_de_gestión_de_inventario():
+    # Crear una cuenta de usuario en el sistema de gestión de inventario
+
+def And_El_usuario_está_en_la_página_de_seguimiento_de_inventario():
+    # Navegar a la página de seguimiento de inventario
+
+def When_El_usuario_ingresa_el_código_del_producto():
+    # Ingresar el código del producto
+
+def Then_El_sistema_muestra_los_niveles_de_existencias_y_stock_del_producto_en_tiempo_real():
+    # Verificar que el sistema muestre los niveles de existencias y stock del producto en tiempo real
